4dc625bd39e0c483aa4751bb7d1a15c50b2690aa
Mengyi Wang <mengyi.wang@canonical.com>
2023-08-23 18:42:55 +0200

    Remove unused files, add $ARG for cli, update copyright, layout /mnt

--- snap/local/bin/run.sh
+++ snap/local/bin/run.sh
@@ -2,5 +2,6 @@
 
 export SERVER_ADDRESS=$(snapctl get server-address)
 export TOPIC_PREFIX=$(snapctl get topic-prefix)
+ARGS=$(snapctl get args)
 
-$SNAP/bin/chip-bridge-app
+$SNAP/bin/chip-bridge-app $ARGS
