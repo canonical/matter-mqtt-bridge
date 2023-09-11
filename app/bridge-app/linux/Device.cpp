d960d11bdfd9ddb5d8b65f95a224bc12599fec11
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-09-11 11:08:44 +0200

    Add Canonical to license headers (#8)

--- app/bridge-app/linux/Device.cpp
+++ app/bridge-app/linux/Device.cpp
@@ -2,6 +2,7 @@
  *
  *    Copyright (c) 2021-2022 Project CHIP Authors
  *    Copyright (c) 2019 Google LLC.
+ *    Copyright (c) 2023 Canonical Ltd.
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
